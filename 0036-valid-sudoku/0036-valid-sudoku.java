07d32091a3899aab84099f48a52bbfe775606194
ManishRed15 <164606204+ManishRed15@users.noreply.github.com>
2025-02-09 14:40:24 -0800

    Time: 2 ms (76.48%), Space: 44.7 MB (41.48%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<Character>[] rowSet = new HashSet[9];
+        Set<Character>[] columnSet = new HashSet[9];
+        Set<Character>[] gridSet = new HashSet[9];
+
+        for(int i=0; i<9; i++){
+            rowSet[i] = new HashSet<>();
+            columnSet[i] = new HashSet<>();
+            gridSet[i] = new HashSet<>();
+        }
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                int gridNo = (i/3)*3 + (j/3);
+
+                if(board[i][j] != '.'){
+                    boolean isPresentInRow = rowSet[i].contains(board[i][j]);
+                    boolean isPresentInColumn = columnSet[j].contains(board[i][j]);
+                    boolean isPresentInGrid = gridSet[gridNo].contains(board[i][j]);
+
+                if(isPresentInRow || isPresentInColumn || isPresentInGrid){
+                    return false;
+                }
+
+                rowSet[i].add(board[i][j]);
+                columnSet[j].add(board[i][j]);
+                gridSet[gridNo].add(board[i][j]);
+            }
+            }
+        }
+        return true;
+    
+    }
+}
