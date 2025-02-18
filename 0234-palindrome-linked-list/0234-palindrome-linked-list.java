19c6e0d85aa328750fc8d9f6fa4167d7cefeacbd
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-02-17 23:47:19 -0800

    Time: 15 ms (9.94%), Space: 57 MB (87.73%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,63 +10,18 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null){
-            return true;
-        }
-        int n = findLength(head);
-        int breakPoint = n/2;
-        if(n%2 != 0){
-            breakPoint = n/2 + 1;
-        }
-        ListNode reverseNode = pointToBreak(head, breakPoint);
-        ListNode prev = pointToBreak(head, breakPoint-1);
-
-        reverseFunction(prev, reverseNode);
-
-        ListNode first = head;
-        ListNode second = prev.next;
-        while(first != null && second != null){
-            if(first.val != second.val){
-                return false;
-            }
-            first = first.next;
-            second = second.next;
-        }
-        return true;
-    }
-
-    private int findLength(ListNode head){
+        StringBuilder str = new StringBuilder();
         ListNode current = head;
-        int length = 0;
-        while(current != null)
-        {
-            current = current.next;
-            length++;
-        }
-        return length;
-    }
 
-    private ListNode pointToBreak(ListNode head, int k)
-    {
-        int count = 0;
-        ListNode current = head;
-        while(count < k){
-            current = current.next;
-            count++;
+        while(current != null){
+            str.append(current.val);
+            current = current.next;  
         }
-        return current;
-    }
 
-    private void reverseFunction(ListNode prev, ListNode reverseNode){
-        ListNode first = reverseNode;
-        ListNode second = first.next;
-        while(first != null && second != null){
-            ListNode temp = second.next;
-            second.next = first;
-            first = second;
-            second = temp;
-        }
-        prev.next.next = null;
-        prev.next = first;
+        String original = str.toString();
+
+        String reversed = str.reverse().toString();
+        return original.equals(reversed);
+        
     }
 }
