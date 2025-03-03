e5d77a7d5d9f0f1d6ebe63fbf314fbff560cc06f
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-02 19:41:55 -0800

    Time: 5 ms (97.67%), Space: 45.2 MB (15.83%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,6 +1,36 @@
+import java.util.Stack;
+
 class Solution {
     public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
 
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    int b = stack.pop(), a = stack.pop();
+                    stack.push(a - b);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    int b2 = stack.pop(), a2 = stack.pop();
+                    stack.push(a2 / b2);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
+
+
+
+/*
         Stack<Integer> stack = new Stack<>();
         for(String token: tokens){
             if(isOperator(token)){
@@ -18,9 +48,9 @@ class Solution {
 
     public boolean isOperator(String token){
         return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
-    }
+    }*/
 
-    public int evaluate(String token, int val1, int val2){
+   /* public int evaluate(String token, int val1, int val2){
         if (token.equals("+"))
             return val1+val2;
         else if (token.equals("-"))
@@ -29,5 +59,4 @@ class Solution {
             return val1*val2;
         else 
             return val1/val2;
-    }
-}
+    }*/
