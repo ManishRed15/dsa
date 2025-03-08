e7b7d40220b566c09257a05b91d0bbb9902bfc00
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-07 17:25:59 -0800

    Time: 4 ms (99.64%), Space: 45.3 MB (84.32%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,5 +1,19 @@
 class Solution {
     public String removeDuplicates(String s) {
+        char[] stack = s.toCharArray();
+        int top = -1;
+        for(char c:stack){
+            if(top>=0 && stack[top]==c){
+                top--;
+            }
+            else{
+            stack[++top] = c;
+            }
+        }
+        return new String(stack,0,top+1);
+    }
+}
+/*
         StringBuilder res = new StringBuilder();
         for(int i=0;i<s.length();i++){
             int len = res.length();
@@ -11,5 +25,4 @@ class Solution {
             }
         }
         return res.toString();
-    }
-}
+        */
