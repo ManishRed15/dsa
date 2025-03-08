3316202a70a4b7219d30aee396d31b18009fb660
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-07 18:57:29 -0800

    Time: 1 ms (99.67%), Space: 42.5 MB (35.14%) - LeetHub

--- 1666-make-the-string-great/1666-make-the-string-great.java
+++ 1666-make-the-string-great/1666-make-the-string-great.java
@@ -1,5 +1,21 @@
 class Solution {
     public String makeGood(String s) {
+        char[] stack = s.toCharArray();
+
+        int top = -1;
+
+        for(char c : stack) {
+            if(top >= 0 && Math.abs(stack[top] - c) == 'a' - 'A' )
+            {
+                top--;
+            }
+            else{
+                stack[++top] = c;
+            }
+        }
+        return new String(stack, 0, top+1);
+
+        /*
         StringBuilder str = new StringBuilder();
 
         for(char c:s.toCharArray()){
@@ -12,5 +28,6 @@ class Solution {
             }
         }
         return str.toString();
+        */
     }
 }
