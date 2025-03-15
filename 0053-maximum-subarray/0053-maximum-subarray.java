cc1b32e5a33366f067a8f2ae69d144ef2bfd885d
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-15 13:10:10 -0700

    Time: 1 ms (99.5%), Space: 56.8 MB (81.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,54 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSoFar = nums[0];
-        int currentSum = nums[0];
-        for(int i=1;i < nums.length;i++){
-            if(currentSum < 0){
-                currentSum = 0;
+        int n = nums.length;
+        int sum = nums[0], maxSum = nums[0];
+
+        for(int i=1; i<n; i++){
+            if(sum<0){
+                sum=0;
             }
-            currentSum = currentSum + nums[i];
-            if(currentSum > maxSoFar){
-                maxSoFar = currentSum;
-            }
-        }
-        return maxSoFar;
         
+            sum = sum + nums[i];
+            maxSum =Math.max(sum, maxSum);
+        }
+        return maxSum;
+
     }
 }
-
-/* 
-class Solution {
-    public int maxSubArray(int[] nums) {
-        int globalMax = Integer.MIN_VALUE;
-        int localMax = 0; 
-        for(int i=0;i<nums.length;i++){
-            localMax=Math.max(nums[i],nums[i]+localMax);
-           globalMax = Math.max(localMax,globalMax);
-        }
-        return globalMax;
-    }
-} */
-/* If you want to return the subarray as well
-
-public int maxSubArray(int[] nums) {
-        int maxSoFar = nums[0];
-        int currentSum = nums[0];
-        int start=0, end=0, tempStart=0;
-
-        for(int i = 1; i < nums.length; i++) {
-            currentSum = currentSum + nums[i];
-            if (currentSum < 0) {
-                currentSum = 0;
-                tempStart = i+1;
-            }
-            if (currentSum > maxSoFar) {
-                maxSoFar = currentSum;
-                start = tempStart;
-                end = i;
-            }
-        }
-        System.out.println("The subarray with maximum sum is: "+ Arrays.toString(Arrays.copyOfRange(nums, start,end+1)));
-        return maxSoFar;
-    }
-
-*/
