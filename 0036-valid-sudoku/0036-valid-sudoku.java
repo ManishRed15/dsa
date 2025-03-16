d60a40bd886bed91a45aee2524594f148bc3ae7c
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-16 14:16:34 -0700

    Time: 2 ms (77.2%), Space: 44.5 MB (67.32%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,22 +1,5 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
-
-     /*   Set set = new HashSet();
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                int gridNum = (i/3)*3 + (j/3);
-                if(board[i][j] != '.'){
-                    i
-                }
-            }
-        } */
-
-
-
-
-
-
-        
+    public boolean isValidSudoku(char[][] board) { 
         Set<Character>[] rowSet = new HashSet[9];
         Set<Character>[] columnSet = new HashSet[9];
         Set<Character>[] gridSet = new HashSet[9];
@@ -39,7 +22,6 @@ class Solution {
                 if(!isPresentInRow || !isPresentInColumn || !isPresentInGrid){
                     return false;
                 }
-
                 //rowSet[i].add(board[i][j]);
                 //columnSet[j].add(board[i][j]);
                 //gridSet[gridNo].add(board[i][j]);
