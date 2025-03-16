2297dc53b60dc7a15b8b6ec13edb0730fb406328
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-16 15:21:50 -0700

    Time: 31 ms (58.2%), Space: 50.9 MB (72.19%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,8 +1,24 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (a,b)-> (a[0]*a[0]+a[1]*a[1])-(b[0]*b[0]+b[1]*b[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a,b) -> ((b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1])));
 
-        return Arrays.copyOfRange(points,0,k);
-        
+        for(int[] point:points){
+            pq.add(point);
+
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+
+        int[][] res = new int[k][2];
+        for(int i=0; i<k; i++){
+            res[i] = pq.poll();
+        }
+
+        return res;
     }
 }
+/* Using simple arrays sort T.C - O(NlogN)
+        Arrays.sort(points, (a,b)-> (a[0]*a[0]+a[1]*a[1])-(b[0]*b[0]+b[1]*b[1]));
+        return Arrays.copyOfRange(points,0,k);*/ 
