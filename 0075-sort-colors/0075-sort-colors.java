d48a1a9335e94e76bde6cef9e5386ad708675198
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-16 19:42:30 -0700

    Time: 0 ms (100%), Space: 42.2 MB (17.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,21 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
+        int left =0, right =nums.length - 1;
         int i=0;
-
-        while(i<= right){
+        while(i<=right){
             if(nums[i]==0){
-                int temp = nums[i];
-                nums[i] = nums[left];
-                nums[left]=temp;
+                int temp = nums[left];
+                nums[left]=nums[i];
+                nums[i]=temp;
                 left++;
-                i++; 
+                i++;
             }
             else if(nums[i]==2){
-                int temp = nums[i];
-                nums[i] = nums[right];
-                nums[right] = temp;
+                int temp = nums[right];
+                nums[right] = nums[i];
+                nums[i] = temp;
                 right--;
             }
             else{
@@ -23,4 +21,31 @@ class Solution {
             }
         }
     }
+             /*
+        int count1=0, count2=0, count3=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                count1++;
+            }
+            else if(nums[i]==1){
+                count2++;
+            }
+            else{
+                count3++;
+            }
+        }
+        for(int i=0; i<count1; i++)
+        {
+            nums[i] = 0;
+        }
+        for(int i=count1; i<count1+count2; i++)
+        {
+            nums[i] = 1;
+        }
+        for(int i=count2+count1; i<nums.length; i++)
+        {
+            nums[i] = 2;
+        }
+    }*/  
 }
+
