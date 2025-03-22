b811fcdc54f9385a82ab3d6a1070647bcdbad0ad
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-21 23:25:46 -0700

    Time: 30 ms (70.61%), Space: 67.7 MB (31.77%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -7,13 +7,13 @@ class Solution {
 
         int ans = 0;
         for(int num : set){
-            int nextNum = num + 1;
+            int nextNum = num + 1; 
             int count = 1;
-            if(!set.contains(num-1)){
-                while(set.contains(nextNum)){
-                    nextNum++;
-                    count++;
-                }
+            if(!set.contains(num-1)){            // T.C = O(N)
+                while(set.contains(nextNum)){    // while(set.contains(nextNum)) loop runs at 
+                    nextNum++;                   //most n times total over the entire array
+                    count++;                     //Because each element is only part of one
+                }                                //sequence and visited once.
             ans = Math.max(ans, count);
             }
         }
