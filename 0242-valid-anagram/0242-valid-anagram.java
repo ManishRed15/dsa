bc1b3a799dbef33b9a343462c199778386be2aae
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-24 22:55:57 -0700

    Time: 14 ms (29.05%), Space: 45 MB (18.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
+        /*
         char[] sArr = s.toCharArray();
         char[] tArr = t.toCharArray();
-
         Arrays.sort(sArr);
         Arrays.sort(tArr);
+        return Arrays.equals(sArr, tArr); */
 
-        return Arrays.equals(sArr, tArr);
+        if(s.length() != t.length()) return false;
 
+        Map<Character, Integer> countMap = new HashMap<>();
+
+        for(char c:s.toCharArray()){
+            countMap.put(c, countMap.getOrDefault(c,0)+1);
+        }
+
+        for(char c:t.toCharArray()){
+            countMap.put(c, countMap.getOrDefault(c,0)-1);
+            if(countMap.get(c) < 0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
