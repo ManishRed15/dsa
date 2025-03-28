8d736b106a1dd401d9583c6d134dce4cbeeef911
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-27 23:13:49 -0700

    Time: 5 ms (97.76%), Space: 44.7 MB (53.58%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,62 +1,27 @@
-import java.util.Stack;
-
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
-
-        for (String token : tokens) {
+        for(String token:tokens){
             switch (token) {
                 case "+":
-                    stack.push(stack.pop() + stack.pop());
+                    stack.push(stack.pop()+stack.pop());
+                    break;
+                case "*":
+                    stack.push(stack.pop()*stack.pop());
                     break;
                 case "-":
                     int b = stack.pop(), a = stack.pop();
-                    stack.push(a - b);
-                    break;
-                case "*":
-                    stack.push(stack.pop() * stack.pop());
+                    stack.push(a-b);
                     break;
                 case "/":
-                    int b2 = stack.pop(), a2 = stack.pop();
-                    stack.push(a2 / b2);
+                    int b1 = stack.pop(), a1 = stack.pop();
+                    stack.push(a1/b1);
                     break;
                 default:
-                    stack.push(Integer.parseInt(token));
+                    stack.push(Integer.parseInt(token)); 
+                
             }
         }
         return stack.pop();
     }
 }
-
-
-
-/*
-        Stack<Integer> stack = new Stack<>();
-        for(String token: tokens){
-            if(isOperator(token)){
-                int val2 = stack.pop();
-                int val1 = stack.pop();
-                int result = evaluate(token, val1, val2);
-                stack.push(result);
-            }
-            else{
-                stack.push(Integer.parseInt(token));
-            }
-        }
-        return stack.pop();        
-    }
-
-    public boolean isOperator(String token){
-        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
-    }*/
-
-   /* public int evaluate(String token, int val1, int val2){
-        if (token.equals("+"))
-            return val1+val2;
-        else if (token.equals("-"))
-            return val1-val2;
-        else if (token.equals("*"))
-            return val1*val2;
-        else 
-            return val1/val2;
-    }*/
