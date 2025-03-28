62c3e3256a72f8fada6107e96a6b6523990a00ff
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-03-27 23:13:57 -0700

    Time: 6 ms (75.17%), Space: 44.9 MB (31.94%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,25 +1,27 @@
+import java.util.Stack;
+
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
-        for(String token:tokens){
+
+        for (String token : tokens) {
             switch (token) {
                 case "+":
-                    stack.push(stack.pop()+stack.pop());
-                    break;
-                case "*":
-                    stack.push(stack.pop()*stack.pop());
+                    stack.push(stack.pop() + stack.pop());
                     break;
                 case "-":
                     int b = stack.pop(), a = stack.pop();
-                    stack.push(a-b);
+                    stack.push(a - b);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
                     break;
                 case "/":
-                    int b1 = stack.pop(), a1 = stack.pop();
-                    stack.push(a1/b1);
+                    int b2 = stack.pop(), a2 = stack.pop();
+                    stack.push(a2 / b2);
                     break;
                 default:
-                    stack.push(Integer.parseInt(token)); 
-                
+                    stack.push(Integer.parseInt(token));
             }
         }
         return stack.pop();
