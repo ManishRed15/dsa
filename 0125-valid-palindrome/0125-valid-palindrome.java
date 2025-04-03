837cefbd298249dfb3ba4d10776aa2f4b9c5a17a
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-04-02 21:15:20 -0700

    Time: 1 ms (100%), Space: 43.4 MB (59.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,44 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0, right = s.length()-1;
+        char[] charArr = new char[s.length()];
+        int i = 0;
+        for(char c:s.toCharArray()){
+            if((c>='0' && c<='9')||(c>='a' && c<='z')){
+                charArr[i] = c;
+                i++;
+            }
+            else if(c >= 'A' && c<='Z'){
+                charArr[i] = (char)(c+32);
+                i++;
+            }
+        }
 
+        int left = 0, right = i-1;
+        while(left<right){
+            if(charArr[left]!=charArr[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+/*
+int left = 0, right = s.length()-1;
         while(left <= right){
             char leftChar = Character.toLowerCase(s.charAt(left));
             char rightChar = Character.toLowerCase(s.charAt(right));
-
-            if(!Character.isLetterOrDigit(leftChar)){
+            if(!Character.isLetterOrDigit(leftChar))
                 left++;
-            }
-            else if(!Character.isLetterOrDigit(rightChar)){
+            else if(!Character.isLetterOrDigit(rightChar))
                 right--;
-            }
-            else if(leftChar != rightChar){
+            else if(leftChar != rightChar)
                 return false;
-            }
             else{
                 left++;
                 right--;
             }
         }
         return true;
-    }
-}
+*/
