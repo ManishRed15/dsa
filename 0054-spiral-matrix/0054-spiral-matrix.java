15a28d93f222c49c5b4aafd62cb8b542b6f7ee7a
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-04-02 23:08:05 -0700

    Time: 0 ms (100%), Space: 41.5 MB (63.69%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,37 +1,33 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<>();
-        int m = matrix.length, n = matrix[0].length;
-        int top=0, right= n-1, left = 0, bottom = m-1;
         int direction = 0;
+        int m = matrix.length, n = matrix[0].length;
+        int top = 0, bottom = m-1, left = 0, right = n-1;
+        List<Integer> res = new ArrayList<>();
 
-        while(top <= bottom && left <= right){
-            if(direction == 0){
-                //print left to right(top)
+        while(top <= bottom && left<=right){
+            if(direction == 0){ //print top left to right
                 for(int j=left; j<=right; j++){
                     res.add(matrix[top][j]);
                 }
-                direction = 1;
                 top++;
+                direction = 1;
             }
-            else if(direction == 1){
-                //print top to bottom(right)
+            else if(direction == 1){//print rightmost top to bottom
                 for(int i=top; i<=bottom; i++){
                     res.add(matrix[i][right]);
                 }
-                direction = 2;
                 right--;
+                direction = 2;
             }
-            else if(direction == 2){
-                //print right to left(bottom)
+            else if(direction == 2){ //print bottom right to left
                 for(int j=right; j>=left; j--){
                     res.add(matrix[bottom][j]);
                 }
-                direction = 3;
                 bottom--;
+                direction = 3;
             }
-            else{
-                //print bottom to top(left)
+            else{ //print bottom to top in the left
                 for(int i=bottom; i>=top; i--){
                     res.add(matrix[i][left]);
                 }
@@ -39,8 +35,6 @@ class Solution {
                 direction = 0;
             }
         }
-
         return res;
-        
     }
 }
