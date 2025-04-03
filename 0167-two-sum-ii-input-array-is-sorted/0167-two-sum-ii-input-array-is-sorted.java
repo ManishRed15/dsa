c4eaab131f939498912f5b3284062d0ab71ba4f2
Manish <164606204+ManishRed15@users.noreply.github.com>
2025-04-02 20:44:25 -0700

    Time: 2 ms (92.6%), Space: 47.1 MB (79.31%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,32 +1,21 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-
-        /* Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0;i < numbers.length;i++){
-            int complement = target - numbers[i];
-            if(map.containsKey(complement)){
-                return new int[] {map.get(complement),i+1};
-            }
-                map.put(numbers[i],i+1);
-        }
-        return null;*/
-
-        //Use Two-Pointer (O(N), O(1)) → When the array is sorted.
-        //Use HashMap (O(N), O(N)) → When the array is not sorted.
-
-
+        int n = numbers.length;
         int left = 0;
-        int right = numbers.length-1;
-
+        int right = n-1;
+        int sum = 0;
         while(left < right){
-            int sum = numbers[left]+numbers[right];
-            if(sum < target)
+            sum = numbers[left] + numbers[right];
+            if(sum < target){
                 left++;
-            else if(sum > target)
+            }
+            else if(sum > target){
                 right--;
-            else
-                return new int[]{left+1, right+1};
+            }
+            else{
+                return new int[]{left+1,right+1};
+            }
         }
-        return null;       
+        return null;
     }
 }
